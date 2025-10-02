c938153481b4fc98eaba8e07438f32ea5432cb7a
Minh Nguyen <mnguyen6@unb.ca>
2025-10-02 15:50:55 -0300

    add a testing gym env and initialize the __init__ method with the observation and action space
    - can handle errors with act_bound of wrong length (can only 2 or 3) or of wrong type (can only be int of float)

---
+++ python/environments/test.py
@@ -0,0 +1,5 @@
+from testing_env import TestingEnv
+
+env = TestingEnv(act_bound = [5,1], 
+                 size = 5, 
+                 render_mode = None)
---
+++ python/environments/testing_env.py
@@ -0,0 +1,71 @@
+from typing import Sequence
+import numpy as np 
+import gymnasium as gym
+
+class TestingEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 30}
+    # TODO - actually create the render somehow
+
+    def __init__(self, 
+                 act_bound: Sequence[int|float], # either a list or tuple of float of x_linear bound, y_linear bound (optional) and z_angular bound
+                 size: float = 5.0,
+                 render_mode = None
+                 ):
+        
+        """ Observation and Action Spaces """
+        self.size = size       # assume 5x5 m^2 environment
+        
+        # --- ensure all elements of act_bound are numeric
+        if not all(isinstance(x, (int, float)) for x in act_bound):
+            raise TypeError("All elements in act_bound must be int or float")
+
+        # --- unpack action_bounds and check length of argument
+        if len(act_bound) == 2:
+            self._is_holonomic = False
+        elif len(act_bound) == 3:
+            self._is_holonomic = True
+        else:
+            raise ValueError("Action bounds must have eiher 2 or 3 elements")
+
+
+        # --- unpack the action bounds
+        if not self._is_holonomic:
+            self.act_x_linear_bound, self.act_z_angular_bound = map(abs, map(float, act_bound))
+        else:
+            self.act_x_linear_bound, self.act_y_linear_bound, self.act_z_angular_bound = map(abs, map(float,act_bound))
+            
+        
+        # --- define the uninitialized location of the agent and the target - to be set randomly in the reset() method
+        self._agent_loc = np.array([0, 0], dtype=np.float32)
+        self._target_loc = np.array([0, 0], dtype=np.float32)
+
+        # --- define the observation space
+        self.observation_space = gym.spaces.Box(
+            low=-size/2,        # [x_min, y_min, target_x_min, target_y_min]
+            high=size/2,        # [x_max, y_max, target_x_max, target_y_max]
+            shape=(4,), dtype=np.float32)
+        
+        # --- define the action space (continuous linear and angular velocity)
+        if not self._is_holonomic:
+            self.action_space = gym.spaces.Box(
+                low = np.array([-self.act_x_linear_bound, -self.act_z_angular_bound], dtype=np.float32),
+                high = np.array([self.act_x_linear_bound, self.act_z_angular_bound], dtype=np.float32),
+                dtype = np.float32
+            )
+        else:
+            self.action_space = gym.spaces.Box(
+                low = np.array([-self.act_x_linear_bound, -self.act_y_linear_bound, -self.act_z_angular_bound]),
+                high = np.array([self.act_x_linear_bound, self.act_y_linear_bound, self.act_z_angular_bound]),
+                dtype = np.float32
+            )
+
+        """ Render mode stuff """
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        self.window = None
+        self.clock = None
+
+    # TODO - create a _get_obs() and _get_info() methods
+    # TODO - create the reset() method
+    # TODO - create the step() method
