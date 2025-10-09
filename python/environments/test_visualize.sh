f31fdb7d255f850d2a4a08f7adf7be3d4017a61c
Minh Nguyen <mnguyen6@unb.ca>
2025-10-09 16:05:05 -0300

    add matt's changes and script to visualize MuJoCo xml

---
+++ python/environments/test_visualize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# # --- this should work 100%
+# python3 -m mujoco.viewer --mjcf=assets/sanity_check.xml
+
+# --- Minh custom MuJoCo environments
+python3 -m mujoco.viewer --mjcf=assets/env_test_minh.xml
---
+++ python/environments/testing_reset.py
@@ -0,0 +1,18 @@
+"""
+this is matt's file for testing mujoco development.
+
+the purpose of this file is for rendering the developed mujoco environment.
+
+"""
+# imports:
+import os
+# import mujoco
+# import mujoco.viewer
+
+# model = mujoco.MjModel.from_xml_path("ROS2_DRL_Navigation/python/environments/assets/env_test.xml")
+# data = mujoco.MjData(model)
+
+# with mujoco.viewer.launch_passive(model, data) as viewer:
+#     while viewer.is_running():
+#         mujoco.mj_step(model, data)
+#         viewer.sync()
