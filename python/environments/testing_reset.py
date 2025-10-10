ebcc119f89baaefd7945d11ae7fcc31005efa92c
MattTidd <mtidd2@unb.ca>
2025-10-09 21:04:19 -0300

    this is the file that matt uses to visualize his environment. going to add the ability to create parameterized envs from code here

--- python/environments/testing_reset.py
+++ python/environments/testing_reset.py
@@ -5,14 +5,16 @@ the purpose of this file is for rendering the developed mujoco environment.
 
 """
 # imports:
-import os
-# import mujoco
-# import mujoco.viewer
+import mujoco
+import mujoco.viewer
 
-# model = mujoco.MjModel.from_xml_path("ROS2_DRL_Navigation/python/environments/assets/env_test.xml")
-# data = mujoco.MjData(model)
+# create model:
+model  = mujoco.MjModel.from_xml_path("python/environments/assets/env_test_matt.xml")
+data = mujoco.MjData(model)
 
-# with mujoco.viewer.launch_passive(model, data) as viewer:
-#     while viewer.is_running():
-#         mujoco.mj_step(model, data)
-#         viewer.sync()
+# render:
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
+
