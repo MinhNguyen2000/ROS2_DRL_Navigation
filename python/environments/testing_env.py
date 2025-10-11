229632b4e6a2a8abb0964016cf0ec0ec13cf53b7
Minh Nguyen <mnguyen6@unb.ca>
2025-10-11 03:53:03 -0300

    remove act_bound argument and action_space handling
    - the action bounds are handled by the MujocoEnv.__init__() function call, which handles setting the action space based on the actuator control range specified in the MJCF xml file

--- python/environments/testing_env.py
+++ python/environments/testing_env.py
@@ -9,14 +9,9 @@ class TestingEnv(MujocoEnv):
     # TODO - actually create the render somehow
 
     def __init__(self, 
-                 act_bound: Sequence[int|float], # either a list or tuple of float of x_linear bound, y_linear bound (optional) and z_angular bound
+                 xml_file: str = 'assets/env_test_minh.xml',
+                 frame_skip: int = 2,
                  size: float = 5.0,
-                 render_mode: str | None = None,
-                 width: int = 480,
-                 height: int = 480,
-                 camera_id: int | None = None,
-                 camera_name: str | None = None,
-                 default_camera_config: dict[str, float | int] | None = None
                  default_camera_config: dict[str, float | int] | None = None,
                  **kwargs
                 #  render_mode: str | None = None,
@@ -29,37 +24,17 @@ class TestingEnv(MujocoEnv):
         """ Observation and Action Spaces """
         self.size = size       # assume 5x5 m^2 environment
         
-        # --- ensure all elements of act_bound are numeric
-        if not all(isinstance(x, (int, float)) for x in act_bound):
-            raise TypeError("All elements in act_bound must be int or float")
-
-        # --- unpack action_bounds and check length of argument
-        if len(act_bound) == 2:
-            self._is_holonomic = False
-        elif len(act_bound) == 3:
-            self._is_holonomic = True
-        else:
-            raise ValueError("Action bounds must have eiher 2 or 3 elements")
-
-
-        # --- unpack the action bounds
-        if not self._is_holonomic:
-            self.act_x_linear_bound, self.act_z_angular_bound = map(abs, map(float, act_bound))
-        else:
-            self.act_x_linear_bound, self.act_y_linear_bound, self.act_z_angular_bound = map(abs, map(float,act_bound))
-            
-        
         # --- define the uninitialized location of the agent and the target - to be set randomly in the reset() method
         self._agent_loc = np.array([0, 0], dtype=np.float32)
         self._target_loc = np.array([0, 0], dtype=np.float32)
 
         # --- define the observation space
-        self.observation_space = gym.spaces.Box(
         observation_space = gym.spaces.Box(
             low=-size/2,        # [x_min, y_min, target_x_min, target_y_min]
             high=size/2,        # [x_max, y_max, target_x_max, target_y_max]
             shape=(4,), dtype=np.float32)
         
+        # --- initialize the simulator (model + data), set action space, and create a renderer
         MujocoEnv.__init__(
             self,
             xml_file=xml_file,
@@ -68,43 +43,9 @@ class TestingEnv(MujocoEnv):
             default_camera_config=default_camera_config,
             **kwargs
         )
-        
-        # --- define the action space (continuous linear and angular velocity)
-        if not self._is_holonomic:
-            self.action_space = gym.spaces.Box(
-                low = np.array([-self.act_x_linear_bound, -self.act_z_angular_bound], dtype=np.float32),
-                high = np.array([self.act_x_linear_bound, self.act_z_angular_bound], dtype=np.float32),
-                dtype = np.float32
-            )
-        else:
-            self.action_space = gym.spaces.Box(
-                low = np.array([-self.act_x_linear_bound, -self.act_y_linear_bound, -self.act_z_angular_bound]),
-                high = np.array([self.act_x_linear_bound, self.act_y_linear_bound, self.act_z_angular_bound]),
-                dtype = np.float32
-            )
-
-        """ Render mode stuff """
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
-        self.render_mode = render_mode
 
         self.window = None
         self.clock = None
-
-        # TODO - load the MuJoCo MJCF/URDF model and create a simulation object MjData
-        self.model = mujoco.MjModel.from_xml_path("assets/env_test_minh.xml")
-        self.data = mujoco.MjData(self.model)
-
-        # TODO - initialize the renderer
-        from gymnasium.envs.mujoco.mujoco_rendering import MujocoRenderer
-        self.renderer = MujocoRenderer(
-            self.model,
-            self.data,
-            default_cam_config=default_camera_config,
-            width=width,
-            heigh=height,
-            camera_id=camera_id,
-            camera_name=camera_name
-        )
 
     # TODO - create a _get_obs() and _get_info() methods
     def _get_obs():
@@ -114,6 +55,7 @@ class TestingEnv(MujocoEnv):
     # TODO - create the step() method
     def step(self, action):
         # 1. move the simulation forward with the TRANSFORMED action (w.r.t. original frame)
+        self.do_simulation(action, self.frame)
         # 2. collect the new observation (LiDAR simulation, location of agent/goal using the custom _get_obs())
         # 3. termination condition
         # 4. reward
