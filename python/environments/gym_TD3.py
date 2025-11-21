96fa99e7de41533fba94ad79fddc75f769fb811e
Minh Nguyen <mnguyen6@unb.ca>
2025-11-21 11:55:28 -0400

    add action_noise on TD3 policy, which is doubled when advancing to a new curriculum to collect transition at boosted exploration

--- python/environments/gym_TD3.py
+++ python/environments/gym_TD3.py
@@ -11,6 +11,7 @@ from stable_baselines3 import TD3,SAC
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.noise import NormalActionNoise
 
 import torch
 import gymnasium as gym
@@ -65,13 +66,15 @@ def main():
         return np.mean(eval_rew_hist).round(3)
 
     class CurriculumCallback(BaseCallback):
-        def __init__(self, success_window = 50, threshold = 0.7):
+        def __init__(self, success_window = 100, threshold = 0.9):
             super().__init__()
             self.success_threshold = threshold
             self.success_window = success_window
             self.buffer = deque(maxlen=success_window)
-            self.goal_bound_levels = np.linspace(0,1,5)
+            self.goal_bound_levels = np.linspace(0,1,9)
             self.goal_level_idx = 0
+
+            self.explore_boost_steps = 0            # keep count of 
 
         def _on_step(self) -> bool:
             # grab info dict from each parallel env
@@ -83,24 +86,54 @@ def main():
 
             if len(self.buffer) >= self.success_window:
                 success_rate = sum(self.buffer)/len(self.buffer)
-                # print(f" Current training success {success_rate:3.2f}", end="\r")
+                print(f"Current training success {success_rate:3.2f}", end="\r")
 
                 # if the success rate of the past N episodes exceed success threshold, progress with curriculum for all env
                 if success_rate > self.success_threshold:
-                    self.increase_goal_bound()
+                    self._advance_curriculum()
                     self.buffer.clear()
 
+            # handle episodes with increased action noise
+            if self.explore_boost_steps > 0:
+                self.explore_boost_steps -= 1
+                if self.explore_boost_steps == 0:
+                    
+                    vec_noise = self.model.action_noise
+                    for i in vec_noise.noises:
+                        i._sigma = self.noise_std_backup.copy()
+                    
+                    print(f"\nFinished with steps of increased exploration, noise reset to {i._sigma}")
             return True     # to continue training, return True, else return False
     
-        def increase_goal_bound(self):
+        def _advance_curriculum(self):
+            ''' Function to advance the curriculum by
+            1. increasing the goal randomization bound 
+            2. boost action_noise to increase exploration for a set number of steps 
+            3. increasing the success window and buffer size to ensure performance consistency
+            '''
+
+            #--- increase the goal bound
             env = self.model.get_env()  # vecnormalized wrapped vec env
             vec = env.venv              # unwrap to vec env
             self.goal_level_idx += 1
-            print(f"\nIncreasing goal bound - level {self.goal_level_idx:2d}")
+            print(f"\nIncreasing goal bound - level {self.goal_level_idx:2d} | {self.goal_bound_levels[self.goal_level_idx]:5.3f}")
             self.goal_level_idx = min(self.goal_level_idx, len(self.goal_bound_levels) - 1)
 
             vec.env_method("_set_goal_bound", self.goal_bound_levels[self.goal_level_idx])
 
+            #--- boost exploration for several steps
+            self.explore_boost_steps = 2_000
+            vec_noise = self.model.action_noise             # VectorizedActionNoise, which is a vector of the action noise (NormalActionNoise)
+            noise = vec_noise.noises                        # list of NormalActionNoise instance of one environment
+            self.noise_std_backup = noise[0]._sigma.copy()  # keep a copy of the original noise stdev
+
+            for i in noise:
+                i._sigma *= 2.0     # double the action noise stdev for all child env
+            print(f"Action noise will be increased to {i._sigma} for {self.explore_boost_steps} steps")
+
+            #--- increase the success window and success buffer length
+            self.success_window = int(self.success_window*1.1)
+            self.buffer = deque(maxlen=self.success_window)
             # # Optional debug
             # vals = vec.get_attr("goal_bound")
             # print("\nUpdated bounds:", vals)
@@ -518,9 +551,9 @@ def main():
         reward_scale= {
                 "rew_head_scale": 14.5,
                 "rew_head_approach_scale": 250.0,
-                "rew_dist_scale": 5.0,
+                "rew_dist_scale": 05.0,
                 "rew_dist_approach_scale": 190.0,
-                "rew_time": -0.25,
+                "rew_time": -00.25,
                 "rew_goal_scale": 5_000.0,
                 "rew_obst_scale": -1_000.0
             }
@@ -532,7 +565,18 @@ def main():
 
         # Environment vectorization
         n_proc = 24
+
+        print("Making subprocess vectorized environments!")
+        env = make_vec_env("Nav2D-v0", 
+                            n_envs=n_proc, 
+                            env_kwargs={"max_episode_steps": 1_000,
+                                        "reward_scale_options": reward_scale,
+                                        "randomization_options": randomization_options
+                                        },
+                            vec_env_cls=SubprocVecEnv, 
+                            vec_env_kwargs=dict(start_method='forkserver'))
     
+        n_actions = env.get_attr("action_space")[0].shape[0]        # obtain the size of the action_space from the list of action_space among the n_proc subprocess envs
 
         # Hyperparameters
         learning_rate = 5e-4
@@ -543,7 +587,7 @@ def main():
         gamma=0.99
         train_freq=2
         gradient_steps=4
-        action_noise=None
+        action_noise=NormalActionNoise(mean=np.zeros(n_actions), sigma=0.03*np.ones(n_actions))     
         n_steps=1
         policy_delay=4
         target_policy_noise=0.1
@@ -559,15 +603,6 @@ def main():
         use_custom_policy = False
         cuda_enabled = True
 
-        print("Making subprocess vectorized environments!")
-        env = make_vec_env("Nav2D-v0", 
-                            n_envs=n_proc, 
-                            env_kwargs={"max_episode_steps": 1_000,
-                                        "reward_scale_options": reward_scale,
-                                        "randomization_options": randomization_options
-                                        },
-                            vec_env_cls=SubprocVecEnv, 
-                            vec_env_kwargs=dict(start_method='forkserver'))
         
         env = VecNormalize(env, training=True, norm_obs=True, norm_reward=True, gamma=gamma)
         
@@ -593,8 +628,8 @@ def main():
         
         # Training code
         # run parameters:
-        number_of_runs = 500
-        steps_per_run = 20_000
+        number_of_runs = 1_000
+        steps_per_run = 50_000
         models_to_save = 10
         model_save_freq = int(number_of_runs / models_to_save)
 
@@ -603,7 +638,7 @@ def main():
         result_number = f"result_{len(os.listdir(base_path)):05d}"
         results_path = os.path.join(base_path, result_number)
 
-        curriculum_callback = CurriculumCallback(success_window=100, threshold=0.7)
+        curriculum_callback = CurriculumCallback(success_window=100, threshold=0.8)
 
         # using model.learn approach:
         for run in tqdm(range(1,number_of_runs+1), ncols = 100, colour = "#33FF00", desc = f"{result_number} training progress"):
@@ -652,15 +687,15 @@ def main():
     # # optuna study on reward scale
     # rew_scale_optuna()
 
-    # optuna study on model hyperparameters
+    # # optuna study on model hyperparameters
     # hyperparam_optuna()
 
-    # optuna study on goal randomzation frequency
-    rand_freq_optuna()
+    # # optuna study on goal randomzation frequency
+    # rand_freq_optuna()
 
-    # # Run one training
-    # for i in range(2):
-    #     train()
+    # Run one training
+    for i in range(1):
+        train()
 
 if __name__=="__main__":
     main()
