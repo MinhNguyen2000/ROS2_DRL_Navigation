4f91c466049d2514961257a054eb837f9a713ce7
MattTidd <mtidd2@unb.ca>
2026-02-13 15:21:15 -0400

    made it so that the user only needs to specify the name of the file, not the path to the file. also set a default world to an empty .sdf

--- ros2_ws/src/agent_bringup/launch/launch_world.py
+++ ros2_ws/src/agent_bringup/launch/launch_world.py
@@ -1,34 +1,29 @@
-import os
-from pathlib import Path
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Path to the URDF and the Gazebo launch files
-    gazebo_launch_file_dir = os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+    # Path to the URDF and the Gazebo launch files:
+    pkg_path = get_package_share_directory("agent_bringup")
+    gz_sim_path = get_package_share_directory("ros_gz_sim")
+    gazebo_launch_file_dir = PathJoinSubstitution([gz_sim_path, "launch", "gz_sim.launch.py"])
 
-    default_world = os.path.join(
-        get_package_share_directory('agent_bringup'),
-        'worlds',
-        'world_1.sdf'
-    )
 
+    # define the launch arguments:
+    world = PathJoinSubstitution([pkg_path, "worlds", LaunchConfiguration("world")])
     world_arg = DeclareLaunchArgument(
-        name='world',
-        default_value=default_world,
-        description="Path of the world to load"
+        name = "world",
+        default_value = "empty_world.sdf",
+        description = "Name of the world to be loaded, defaulting to empty_world.sdf"
     )
 
-    world = LaunchConfiguration('world')
-
+    # define the nodes to be launched:
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gazebo_launch_file_dir]),
-        launch_arguments={'gz_args': ['-r -v4 ', world]}.items()
+        launch_arguments={"gz_args": ["-r -v1 ", world]}.items()
     )
 
     return LaunchDescription([
