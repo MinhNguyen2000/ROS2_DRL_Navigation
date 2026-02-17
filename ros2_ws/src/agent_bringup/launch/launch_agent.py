bc9be1d165379ce5b9d2ef8e271c8d1651b7a801
MattTidd <mtidd2@unb.ca>
2026-02-17 13:46:56 -0400

    added launch argument for switching between ros2_control and gazebo plugin control. added ros_gz_bridge for driving simulation. added parameters for ros_gz_bridge. added dynamic namespacing of parameter .yaml file

--- ros2_ws/src/agent_bringup/launch/launch_agent.py
+++ ros2_ws/src/agent_bringup/launch/launch_agent.py
@@ -1,3 +1,5 @@
+import os
+import yaml
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -16,6 +18,7 @@ def generate_launch_description():
     pkg_path = get_package_share_directory("agent_bringup")
     xacro_path = PathJoinSubstitution([pkg_path, "urdf", "agent.urdf.xacro"])
     gazebo_launch_path = PathJoinSubstitution([pkg_path, "launch", "launch_world.py"])
+    bridge_path = os.path.join(pkg_path, "config", "gz_bridge.yaml")
 
     # define the launch arguments:
     use_sim_time = LaunchConfiguration("use_sim_time")
@@ -23,6 +26,13 @@ def generate_launch_description():
         "use_sim_time",
         default_value = "true",
         description = "Whether or not to use sim time, defaulting to false."
+    )
+
+    use_ros_control = LaunchConfiguration("use_ros_control")
+    use_ros_control_arg = DeclareLaunchArgument(
+        "use_ros_control",
+        default_value = "false",
+        description = "If true, control the agent using ros2_control, otherwise, use Gazebo plugins. Defaults to false"
     )
 
     world = PathJoinSubstitution([pkg_path, "worlds", LaunchConfiguration("world")])
@@ -34,8 +44,26 @@ def generate_launch_description():
 
     # set the required parameters:
     agent_name = "agent"
-    robot_description = Command(["xacro ", xacro_path, " agent_name:=", agent_name])
+    robot_description = Command(["xacro ", xacro_path, " agent_name:=", agent_name, " use_ros_control:=", use_ros_control])
     rsp_parameters = {"robot_description": ParameterValue(robot_description, value_type = str), "use_sim_time" : use_sim_time}
+
+    # load the yaml file:
+    with open(bridge_path, "r") as f:
+        yaml_params = yaml.safe_load(f)
+
+    # modify to reflect namespacing:
+    for entry in yaml_params:
+        # name_space the joint_states:
+        if entry.get("gz_topic_name") == "joint_states":
+            entry["ros_topic_name"] = f"{agent_name}/joint_states"
+
+        # # unflatten the tf topic in gazebo:
+        # if entry.get("gz_topic_name") == "tf":
+        #     entry["gz_topic_name"] = f"model/{agent_name}/tf"
+
+    # write back to file:
+    with open(bridge_path, "w") as f:
+        yaml.safe_dump(yaml_params, f)
 
     # define the nodes to be launched:
     rsp = Node(
@@ -60,11 +88,19 @@ def generate_launch_description():
         output = "screen"
     )
 
+    ros_gz_bridge = Node(
+        package = "ros_gz_bridge",
+        executable = "parameter_bridge",
+        arguments = ["--ros-args", "-p", f"config_file:={bridge_path}"]
+    )
+
     return LaunchDescription([
         use_sim_time_arg,
+        use_ros_control_arg,
         world_arg,
         rsp, 
         gazebo,
-        spawner
+        spawner, 
+        ros_gz_bridge
     ])
     
