0460050a43a0f692d08b486b3e7103147ec8c6e7
MattTidd <mtidd2@unb.ca>
2026-02-19 17:04:27 -0400

    added ros2_control. modified launch file so that the user can specify whether to use ros2_control or not. set up controllers for ros2_control, and created separate ros_gz_bridge parameter files. changed friction parameters mu1 and mu2 so that the agent motion in rviz2 best reflects that of Gazebo

--- ros2_ws/src/agent_bringup/launch/launch_agent.py
+++ ros2_ws/src/agent_bringup/launch/launch_agent.py
@@ -1,7 +1,7 @@
 import os
-import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, LogInfo
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
@@ -13,13 +13,16 @@ from ament_index_python.packages import get_package_share_directory
 # - gazebo
 # - spawner
 # - ros_gz_bridge
+# - diff_drive_spawner
+# - joint_broadcaster_spawner
 
 def generate_launch_description():
     # set the required paths:
     pkg_path = get_package_share_directory("agent_bringup")
     xacro_path = PathJoinSubstitution([pkg_path, "urdf", "agent.urdf.xacro"])
     gazebo_launch_path = PathJoinSubstitution([pkg_path, "launch", "launch_world.py"])
-    bridge_path = os.path.join(pkg_path, "config", "gz_bridge.yaml")
+    bridge_path_1 = os.path.join(pkg_path, "config", "gz_bridge_ros_control.yaml")
+    bridge_path_2 = os.path.join(pkg_path, "config", "gz_bridge_gazebo_control.yaml")
 
     # define the launch arguments:
     use_sim_time = LaunchConfiguration("use_sim_time")
@@ -45,22 +48,9 @@ def generate_launch_description():
 
     # set the required parameters:
     agent_name = "agent"
+    
     robot_description = Command(["xacro ", xacro_path, " agent_name:=", agent_name, " use_ros_control:=", use_ros_control])
     rsp_parameters = {"robot_description": ParameterValue(robot_description, value_type = str), "use_sim_time" : use_sim_time}
-
-    # load the yaml file:
-    with open(bridge_path, "r") as f:
-        yaml_params = yaml.safe_load(f)
-
-    # modify to reflect namespacing:
-    for entry in yaml_params:
-        # name_space the joint_states:
-        if entry.get("gz_topic_name") == "joint_states":
-            entry["ros_topic_name"] = f"/{agent_name}/joint_states"
-
-    # write back to file:
-    with open(bridge_path, "w") as f:
-        yaml.safe_dump(yaml_params, f)
 
     # define the nodes to be launched:
     rsp = Node(
@@ -85,11 +75,43 @@ def generate_launch_description():
         output = "screen"
     )
 
-    ros_gz_bridge = Node(
+    ros_gz_bridge_ros_control = Node(
         package = "ros_gz_bridge",
         executable = "parameter_bridge",
-        arguments = ["--ros-args", "-p", f"config_file:={bridge_path}"]
+        name = "ros_gz_bridge_ros_control",
+        arguments = ["--ros-args", "-p", f"config_file:={bridge_path_1}"],
+        condition = IfCondition(use_ros_control)
     )
+
+    ros_gz_bridge_gazebo_control = Node(
+        package = "ros_gz_bridge",
+        executable = "parameter_bridge",
+        name = "ros_gz_bridge_gazebo_control",
+        arguments = ["--ros-args", "-p", f"config_file:={bridge_path_2}"],
+        condition = UnlessCondition(use_ros_control)
+    )
+
+    diff_drive_spawner = Node(
+        package = "controller_manager",
+        executable = "spawner",
+        condition = IfCondition(use_ros_control),
+        arguments = ["diff_controller"],
+    )
+
+    joint_broadcaster_spawner = Node(
+        package = "controller_manager",
+        executable = "spawner",
+        condition = IfCondition(use_ros_control),
+        arguments = ["joint_broad"], 
+    )
+
+    # lidar_transform_publisher = Node(
+    #     package = "tf2_ros",
+    #     executable = "static_transform_publisher",
+    #     name = "lidar_publisher_node",
+    #     arguments = ["0.0405", "5.25826986680105E-05", "0.154", "0", "0", "0", f"{agent_name}_base_link", f"{agent_name}_lidar_link"],
+    #     condition = IfCondition(use_ros_control)
+    # )
 
     return LaunchDescription([
         use_sim_time_arg,
@@ -98,6 +120,10 @@ def generate_launch_description():
         rsp, 
         gazebo,
         spawner, 
-        ros_gz_bridge
+        ros_gz_bridge_ros_control,
+        ros_gz_bridge_gazebo_control,
+        diff_drive_spawner,
+        joint_broadcaster_spawner,
+        # lidar_transform_publisher
     ])
     
