3025a5c384fb9e7078fd12fca7ad3d211040bfe0
Minh Nguyen <mnguyen6@unb.ca>
2026-03-18 20:37:07 +0000

    modify EKF fusion and (co)variance) + Gazebo property changes (wheel vel + LiDAR angle range)
    - removed yaw and ay estimation from wheel odometry
    - used linear x + y velocities of lidar odom in EKF fusion improved the odometry and addressed the jumpy update after a long command pause
    - removed ax fusion from IMU
    
    - increased max wheel vel from 10.0 to 30.0
    - increased mu2
    - changed the LiDAR min-max angle range
    
    - tuned covariance to make lidar_odom's pose and twist covariance tighter

--- ros2_ws/src/covariance_filter/covariance_filter/covariance_filter.py
+++ ros2_ws/src/covariance_filter/covariance_filter/covariance_filter.py
@@ -43,31 +43,31 @@ class CovFilter(Node):
         # orientation covariance row majors, in the order of (roll, pitch, yaw)
         self.imu_orientation_covariance = [1.0, 0.0, 0.0,
                                            0.0, 1.0, 0.0,
-                                           0.0, 0.0, 0.05]
+                                           0.0, 0.0, 1.0]
         # angular velocity covariance row majors, in the order of (vroll, vpitch, vyaw)
         self.imu_angular_velocity_covariance = [1.0, 0.0, 0.0,
                                                 0.0, 1.0, 0.0,
-                                                0.0, 0.0, 0.01]
+                                                0.0, 0.0, 1e-2]
         # linear acceleratioin covariance row majors, in the order of (ax, ay, az)
-        self.imu_linear_acceleration_covariance = [0.1, 0.0, 0.0,
-                                                   0.0, 0.1, 0.0,
-                                                   0.0, 0.0, 0.5]
+        self.imu_linear_acceleration_covariance = [1e-2, 0.0, 0.0,
+                                                   0.0, 1e-2, 0.0,
+                                                   0.0, 0.0, 1e-2]
         
         # LiDAR covariances - as per https://docs.ros2.org/foxy/api/nav_msgs/msg/Odometry.html
         # pose covariance, in the order of (x, y, z, r, p, y)
-        self.lidar_pose_covariance = [0.001,  0.0,   0.0, 0.0, 0.0, 0.0,
-                                      0.0,    0.001, 0.0, 0.0, 0.0, 0.0,
+        self.lidar_pose_covariance = [1e-4,   0.0,   0.0, 0.0, 0.0, 0.0,
+                                      0.0,    1e-4,  0.0, 0.0, 0.0, 0.0,
                                       0.0,    0.0,   1.0, 0.0, 0.0, 0.0,
                                       0.0,    0.0,   0.0, 1.0, 0.0, 0.0,
                                       0.0,    0.0,   0.0, 0.0, 1.0, 0.0,
-                                      0.0,    0.0,   0.0, 0.0, 0.0, 0.05]
+                                      0.0,    0.0,   0.0, 0.0, 0.0, 1e-4]
         # twist covariance, in the order of (vx, vy, vz, vr, vp, vy)
-        self.lidar_twist_covariance = [0.1, 0.0,  0.0,  0.0,  0.0,  0.0,
-                                      0.0,  0.1,  0.0,  0.0,  0.0,  0.0,
+        self.lidar_twist_covariance = [1e-2, 0.0,  0.0,  0.0,  0.0,  0.0,
+                                      0.0,  1e-2,  0.0,  0.0,  0.0,  0.0,
                                       0.0,  0.0,  1.0,  0.0,  0.0,  0.0,
                                       0.0,  0.0,  0.0,  1.0,  0.0,  0.0,
                                       0.0,  0.0,  0.0,  0.0,  1.0,  0.0,
-                                      0.0,  0.0,  0.0,  0.0,  0.0,  0.05]
+                                      0.0,  0.0,  0.0,  0.0,  0.0,  1e-2]
 
     def imu_callback(self, msg):
         msg_fixed = copy.deepcopy(msg)
