e279c94574c232788417e35b8260abcada95fc52
Minh Nguyen <mnguyen6@unb.ca>
2026-03-22 21:00:54 +0000

    add goal points in the navigation visualization

--- ros2_ws/scripts/visualize_path.py
+++ ros2_ws/scripts/visualize_path.py
@@ -1,25 +1,42 @@
 import os, json
 import matplotlib.pyplot as plt
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-PATH_DIR = os.path.join(CURRENT_DIR, '..', 'src', 'drl_policy', 'recorded_paths')
+CURRENT_DIR         = os.path.dirname(os.path.abspath(__file__))
+SAVED_PATHS_DIR     = os.path.join(CURRENT_DIR, '..', 'src', 'drl_policy', 'recorded_paths')
+DEFAULT_PATHS_PATH   = os.path.join(CURRENT_DIR, '..', 'src', 'drl_policy', 'paths', 'paths.json')
 
 path_list = ['SAC_001_path_1', 'TD3_002_path_1']
 
 def main():
     plt.figure(figsize=(10,10))
+
+    # --- Visualize the path ---
     for path in path_list:
-        PATH_PATH = os.path.join(PATH_DIR, f'{path}.json')
+        PATHS_PATH = os.path.join(SAVED_PATHS_DIR, f'{path}.json')
         MODEL_NAME = path.split('_')[0]
 
-        with open(PATH_PATH, 'r') as f:
+        with open(PATHS_PATH, 'r') as f:
             poses = json.load(f)["poses"]
 
         x   = [pose.get('x', float('nan')) for pose in poses]
         y   = [pose.get('y', float('nan')) for pose in poses]
         yaw = [pose.get('yaw', float('nan')) for pose in poses]
 
-        plt.scatter(x, y, s=5, label=MODEL_NAME)
+        plt.plot(x, y, '--', lw=2, label=MODEL_NAME)
+
+    # --- TODO: Visualize the obstacles ---
+
+    # --- Visualize the goals ---
+    path_name = path_list[0].split('_')[-2:]
+    path_name = "_".join(path_name)
+    print(f'Comparing the policy performance on {path_name}')
+    with open(DEFAULT_PATHS_PATH, 'r') as f:
+        AVAILABLE_PATHS = json.load(f)
+    goal_x = [goal[0] for goal in AVAILABLE_PATHS[path_name]]
+    goal_y = [goal[1] for goal in AVAILABLE_PATHS[path_name]]
+    goal_tolerance = 0.1        # meter
+    goal_scatter_size = 0.1 * 100 / (2.54 / 72)     # one scatter size = 1/72 of an inch
+    plt.scatter(goal_x, goal_y, linewidths=1, edgecolors="#1C7826", s=goal_scatter_size, c="#3EA047", alpha=0.5)
 
     plt.legend()
     plt.show()
